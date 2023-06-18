e9b2aa4c1e6ac82bc1202fac7d1802f99b399bf3
Shemroy Caple <shemroycaple@gmail.com>
2023-06-18 15:25:57 -0400

    Added the decrypt function and fixed the overflow problem more efficiently

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,4 +1,5 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
 
@@ -7,32 +8,32 @@ def encrypt(message, key):
     for letter in message:
         letter_index=alphabet.index(letter)
         letter_index+=key
-        
-        if letter_index > 25:
-            letter_index=0
-            letter_index+=key
-
         letter=alphabet[letter_index]
         code+=letter
         
     print(f"Your encrypted message is {code}, the receiptient must use {key} to decrypt it")
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(message, key):
+    code=""
+    for letter in message:
+         letter_index=alphabet.index(letter)
+         letter_index-=key
+         letter = alphabet[letter_index]
+         code+=letter
+    print(f"your hidden message is {code}")
 
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
 
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+    
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+choice = input("Type encode to create a message or decode to decrypt a message: ")
+if choice == "encode":
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    encrypt(text,shift)
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-#direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-encrypt(text,shift)
+elif choice == "decode":
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    decrypt(text,shift)
+else:
+    print("Error: Invalid Choice")
