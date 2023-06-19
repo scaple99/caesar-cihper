834679752bb68460e8e9b6f2c511dc1a9ef7b8e1
Shemroy Caple <shemroycaple@gmail.com>
2023-06-19 04:38:05 -0400

    Started refining the code, will merge with main branch once completed

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,39 +1,20 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
-
-def encrypt(message, key):
-    code =  ""
-    for letter in message:
-        letter_index=alphabet.index(letter)
-        letter_index+=key
-        letter=alphabet[letter_index]
-        code+=letter
-        
-    print(f"Your encrypted message is {code}, the receiptient must use {key} to decrypt it")
-
-def decrypt(message, key):
-    code=""
-    for letter in message:
-         letter_index=alphabet.index(letter)
-         letter_index-=key
-         letter = alphabet[letter_index]
-         code+=letter
-    print(f"your hidden message is {code}")
-
-
+def caesar(text, shift_amount, choice):
+    choice = input("Choose what you want to do: encode or decode?: ")
+    text = input("Enter your text: ")
+    shift_amount = input("Enter the key: ")
+    if choice is not "decode" or "encode":
+        print("Invalid Choice.")
+    plain_text=""
+    if choice == "decode":
+        shift_amount*= -1
+    for letter in text:
+        letter_position=alphabet.index(letter)
+        new_position = letter_position + shift_amount
+        letter = alphabet[new_position]
+        plain_text+=letter
+        print(f"Your {choice}d message is {plain_text}")
     
-
-choice = input("Type encode to create a message or decode to decrypt a message: ")
-if choice == "encode":
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    encrypt(text,shift)
-
-elif choice == "decode":
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    decrypt(text,shift)
-else:
-    print("Error: Invalid Choice")
+    
+caesar(text=message, shift_amount=key,choice=direction)
